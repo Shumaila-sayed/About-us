0a1092488a75af5947845eea1ae8715ad9bfc011
Shumaila <sayedshumaila27@gmail.com>
2025-08-08 14:57:57 +0530

    Fix mission section

--- src/components/about/about-our-mission/Mission.tsx
+++ src/components/about/about-our-mission/Mission.tsx
@@ -3,14 +3,17 @@ import missionData from '../../../data/about/about-page-data.json'
 export default function Mission() {
 	return (
 		<section className='bg-white py-12 px-6 text-center'>
-			<h2 className='text-2xl md:text-4xl font-semibold text-[#2C2C2C] mb-4'>
+			<h2 className='text-3xl inline md:text-4xl font-bold text-[#2C2C2C] '>
 				{missionData.aboutOurMission.sectionHeading}
 			</h2>
-			<h3 className='text-2xl md:text-4xl font-semibold text-[#2C2C2C] mb-4'>
+			<h3 className='text-3xl inline md:text-4xl text-[#2C2C2C] '>
 				{missionData.aboutOurMission.subHeading}
 			</h3>
-			<p className='text-gray-600 max-w-4xl mx-auto text-base md:text-lg leading-relaxed mb-4'>
+			<p className='text-gray-600 max-w-4xl mx-auto text-base md:text-lg leading-relaxed mb-4 mt-8'>
 				{missionData.aboutOurMission.paragraph}
+			</p>
+			<p className='text-gray-600 max-w-4xl mx-auto text-base md:text-lg leading-relaxed mb-4'>
+				{missionData.aboutOurMission.subParagraph}
 			</p>
 			<p className='font-semibold text-[#1A1A1A] text-lg md:text-xl'>
 				{missionData.aboutOurMission.subText}
